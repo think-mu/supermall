05869bbace643d0400afc40f57d2642d8166961d
siem <1138793164@qq.com>
2019-09-29 17:46:57 +0800

    base structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
+    <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
-
-export default {
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+  export default {
   name: 'app',
   components: {
+    MainTabBar
   }
+
 }
 </script>
 
 <style>
-@import "./assets/css/base.css"
+  @import "assets/css/base.css";
 </style>
