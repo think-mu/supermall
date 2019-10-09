2534f23a5ba7276ca31763d435313901e006b41d
siem <1138793164@qq.com>
2019-10-09 17:36:04 +0800

    add data

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,43 +1,121 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control" @tabClick="tabClick"></tab-control>
+    <goods-list :goods="showGoods" />
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import {getHomeMultidata} from "network/home"
-import HomeSwiper from './childComps/HomeSwiper'
-import RecommendView from './childComps/RecommendView'
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+
+import { getHomeMultidata, getHomeGoods } from "network/home";
+
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 
 export default {
-  name:"Home",
+  name: "Home",
   components: {
     NavBar,
+    TabControl,
+    GoodsList,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
       banners: [],
-      recommends: []
-    }
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      currentType: 'pop'
+    };
   },
   created() {
-    getHomeMultidata().then(res => {
-      // console.log(res)
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list; 
-    })
+    this.getHomeMultidata();
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  computed: {
+    showGoods(){
+      return this.goods[this.currentType].list
+    }
+  },
+  methods: {
+    /**
+     * 事件监听
+     */
+    
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    /**
+     * 网络请求
+     */
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        // console.log(res)
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      console.log(1);
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        console.log(2);
+
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-  .home-nav {
-    background-color: var(--color-tint);
-    color: #fff;
-  }
+#home {
+  padding-top: 44px;
+  /* height: 100vh;
+    position: relative; */
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
 </style>
--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container">
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
   </div>
 </template>
 
@@ -15,5 +18,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .feature img{
+    width: 100%;
+  }
 </style>
